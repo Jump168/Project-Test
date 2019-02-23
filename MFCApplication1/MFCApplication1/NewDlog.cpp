b4ef918f0111280ae0ad00ed45b7779991999ad3
XB-201610220909\Administrator <1129067882@qq.com>
2019-02-23 12:21:49 +0800

    回滚后的修改

---
+++ MFCApplication1/MFCApplication1/NewDlog.cpp
@@ -0,0 +1,34 @@
+// NewDlog.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "NewDlog.h"
+#include "afxdialogex.h"
+
+
+// CNewDlog 对话框
+
+IMPLEMENT_DYNAMIC(CNewDlog, CDialogEx)
+
+CNewDlog::CNewDlog(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_DIALOG2, pParent)
+{
+
+}
+
+CNewDlog::~CNewDlog()
+{
+}
+
+void CNewDlog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CNewDlog, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CNewDlog 消息处理程序
